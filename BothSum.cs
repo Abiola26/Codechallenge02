aaee54ad56593d4ddfa28abd4b55ff3414c9fbf9
HP <HP@BIOTECH>
2022-09-27 13:29:34 +0100

    Add project files.

---
+++ BothSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codechallenge02
+{
+    public class BothSum
+    {
+        public int SumArray(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int smallest = array[0];
+            int Largest = array[0];
+            for (int i = 0; i < length; i++)
+            {
+                if (smallest > array[i])
+                {
+                    smallest = array[i];
+                }
+            }
+            for (int i = 0; i < length; i++)
+            {
+                if (Largest < array[i])
+                {
+                    Largest = array[i];
+                }
+            }
+
+            int sum = Largest + smallest;
+
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Codechallenge02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChange02
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            BothSum arraySum = new BothSum();
+
+            Console.WriteLine("Enter the size of the array");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[x];
+            Console.WriteLine("Enter the elements of the array");
+
+
+            int sum = arraySum.SumArray(array);
+            Console.WriteLine($"The input array");
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"The sum of largest and smallest is {sum}");
+        }
+    }
+}
